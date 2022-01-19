e8d8c4b6c6ddc0d14a4ec0d0f1574684c310e8e5
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2022-01-19 17:12:37 -0600

    changed origin

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://safe-stream-65170.herokuapp.com/",
+    origin: "http://localhost:3000",
     // credentials: true,
   },
 });
